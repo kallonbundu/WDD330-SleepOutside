69216895b930aded3aff488edcbff01238552597
Luis Lizano <lewislizano@gmail.com>
2025-05-22 16:10:03 -0500

    Implement header and footer loading functionality with template rendering

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -37,3 +37,28 @@ export function renderListWithTemplate(template, parentElement, list, position =
   }
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
+
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML = template;
+  if (callback) {
+    callback(data);
+  }
+}
+
+export async function loadTemplate(path) { 
+  const response = await fetch(path);
+  const template = await response.text();
+  return template;
+
+}
+
+export async function loadHeaderFooter() {
+  const header = await loadTemplate("../partials/header.html");
+  const headerElement = document.querySelector("#start-header");
+  renderWithTemplate(header, headerElement)
+
+  const footer = await loadTemplate("../partials/footer.html");
+  const footerElement = document.querySelector("#end-footer");
+  renderWithTemplate(footer, footerElement)
+
+}
