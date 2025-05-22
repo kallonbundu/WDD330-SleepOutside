1e2d7de06bfd9983d7a059fde98be5e47ffaff1f
Luis Lizano <lewislizano@gmail.com>
2025-05-22 16:32:59 -0500

    Refactor HTML structure and improve formatting in various files

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
-import {loadHeaderFooter} from './utils.mjs';
-import ProductData from './ProductData.mjs';
-import ProductList from './ProductList.mjs';
+import { loadHeaderFooter } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
 
-const productData =  new ProductData("tents");
+const productData = new ProductData("tents");
 
 const element = document.querySelector(".product-list");
 
-const productList = new ProductList("tents", productData, element)
+const productList = new ProductList("tents", productData, element);
 
 productList.init();
 loadHeaderFooter();
