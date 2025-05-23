f8ca7c08ec2b9e261fdc10fded8a7cbc620cc0bc
stratoverus <keitheberhard93@gmail.com>
2025-05-22 18:05:35 -0700

    initial file commits

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
         product1: resolve(__dirname, "src/product_pages/index.html"),
         header: resolve(__dirname, "src/partials/header.html"),
         footer: resolve(__dirname, "src/partials/footer.html"),
+        product2: resolve(__dirname, "src/product_listing/index.html"),
       },
     },
   },
